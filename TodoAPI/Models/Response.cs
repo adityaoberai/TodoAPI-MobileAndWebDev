e56ceef47fe3570883af6451280213e97d7f55ed
Aditya Oberai <adityaoberai1@gmail.com>
2021-08-06 15:16:48 +0530

    Added new response model

---
+++ TodoAPI/Models/Response.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    public class Response
+    {
+        public string Message { get; set; }
+        public HttpStatusCode Status { get; set; }
+    }
+
+    public class TodoResponse : Response
+    {
+        public Todo Response { get; set; }
+    }
+
+    public class TodoListResponse : Response
+    {
+        public List<Todo> Response { get; set; }
+    }
+}
